919beb6f3f77e504d1c3ad93d3982e63de4d19d9
空 <the_sky.777@icloud.com>
2025-10-16 15:14:16 +0900

    コミット1: 自身の日記データ

---
+++ diaries/SoraDiary.py
@@ -0,0 +1,13 @@
+from diaries.AbstractDiary import AbstractDiary
+
+# 日記データを設定するクラス
+class SoraDiary(AbstractDiary):
+    def get_date(self):
+        return "2024-10-16"
+
+    def get_summary(self):
+        return "今日は、オブジェクト指向及びプログラム演習の授業があった。GitHubの使い方について学んだが、" \
+        "まだ完全には理解できないので、復習が必要だと感じた。"
+
+    def get_author(self):
+        return "Sora"

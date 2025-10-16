e0cd0a6655dbb5f1c0a353ba404bac06e4bc0514
kazuki404313 <kazuki20050404@gmail.com>
2025-10-16 15:43:36 +0900

    Add diary

---
+++ diaries/KazukiDiary.py
@@ -0,0 +1,11 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class KazukiDiary(AbstractDiary):
+    def get_date(self):
+        return "2025-10-q6"
+
+    def get_summary(self):
+        return "Gitめちゃ難しくね"
+
+    def get_author(self):
+        return "Kazuki"
